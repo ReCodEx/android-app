d59214685bd8a0507198b1765c7e42f200e93236
Petr Stefan <ptr.stef@gmail.com>
2017-02-13 23:27:47 +0100

    Add basic navigation drawer (sliding menu)

--- app/src/main/java/io/github/recodex/android/SplashActivity.java
+++ app/src/main/java/io/github/recodex/android/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, NavigationDrawer.class);
         startActivity(intent);
         finish();
     }
